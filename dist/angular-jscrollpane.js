3d0d55827249417372860d4e4447988390226fa7
Prateek Ojha <prateek479@gmail.com>
2014-12-05 14:06:22 +0530

    dynamic width for scroll pane

--- dist/angular-jscrollpane.js
+++ dist/angular-jscrollpane.js
@@ -1,27 +1,29 @@
 'use strict';
 
 (function() {
-  angular.module("ngJScrollPane", []);
+  angular.module('ngJScrollPane', []);
 
-  angular.module("ngJScrollPane").directive("scrollPane", [
+  angular.module('ngJScrollPane').directive('scrollPane', [
     '$timeout',
     function($timeout) {
       return {
         restrict: 'A',
         transclude: true,
-        template: '<div class="scroll-pane"><div ng-transclude></div></div>',
+        template: '<div class="scroll-pane"><div ng-transclude class="scroll-container" style="height : 300px !important"></div></div>',
         link: function($scope, $elem, $attrs) {
+          var cal_width;
+          var config, fn;
+          config = {};
           $scope.$on('albumCompleteEvent', function(event, args) {
-            var cal_width = (args.albumLength * 420) + 'px';
-            $elem.children().css('width', cal_width);
-            $elem.addClass('album-view-scrollpane');
-            var config, fn;
-            config = {};
+            console.log(args.albumLength + 'shi');
+            cal_width = (args.albumLength * 420) + 'px';
+            $('.scroll-container').css('width', cal_width);
+            console.log('s' + $elem.children().width());
             if ($attrs.scrollConfig) {
               config = $scope.$eval($attrs.scrollConfig);
             }
             fn = function() {
-              return jQuery("#" + $attrs.id).jScrollPane(config);
+              return jQuery('#' + $attrs.id).jScrollPane(config);
             };
             if ($attrs.scrollTimeout) {
               return $timeout(fn, $scope.$eval($attrs.scrollTimeout));
