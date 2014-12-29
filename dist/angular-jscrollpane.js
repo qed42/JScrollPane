c79610356e294126d3ec52608737d13f81a99322
Pratik Bothra <pratik60@gmail.com>
2014-12-29 22:52:35 +0530

    Update angular-jscrollpane.js

--- dist/angular-jscrollpane.js
+++ dist/angular-jscrollpane.js
@@ -9,16 +9,15 @@
       return {
         restrict: 'A',
         transclude: true,
-        template: '<div class="scroll-pane"><div ng-transclude class="scroll-container" style="height : 300px !important"></div></div>',
+        template: '<div class="scroll-pane"><div ng-transclude class="scroll-container"></div></div>',
         link: function($scope, $elem, $attrs) {
           var cal_width;
           var config, fn;
           config = {};
           $scope.$on('albumCompleteEvent', function(event, args) {
-            console.log(args.albumLength + 'shi');
+
             cal_width = (args.albumLength * 420) + 'px';
             $('.scroll-container').css('width', cal_width);
-            console.log('s' + $elem.children().width());
             if ($attrs.scrollConfig) {
               config = $scope.$eval($attrs.scrollConfig);
             }
@@ -31,6 +30,19 @@
               return fn();
             }
           });
+          if ($attrs.directInitiate) {
+            if ($attrs.scrollConfig) {
+              config = $scope.$eval($attrs.scrollConfig);
+            }
+            fn = function() {
+              return jQuery('#' + $attrs.id).jScrollPane(config);
+            };c
+            if ($attrs.scrollTimeout) {
+              return $timeout(fn, $scope.$eval($attrs.scrollTimeout));
+            } else {
+              return fn();
+            }
+          }
         },
         replace: true
       };
